d521a0e6ae2966ff7587920f79e20e17a4ab597d
jaGdish7898 <93571870+jaGdish7898@users.noreply.github.com>
2022-01-05 23:17:24 +0530

    todo list update

---
+++ src/ComplateButton.js
@@ -0,0 +1,15 @@
+
+
+export const ComplateButton = () => {
+    
+    return (
+        <>
+            <input id='complate' type='button' value="complete" />
+        
+            
+        </>
+    )
+}
+
+
+//onClick={aaa(ele.id)} 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,72 @@
 
 import { useState } from "react"
 
+import {ComplateButton} from "./ComplateButton"
+import ClearIcon from '@material-ui/icons/Clear';
+// import DeleteIcon from '@material-ui/icons/Delete';
 
 export const TodoList = () => {
-    const [data, dataUpdater]=useState(["work 4 hr","meeting at 5pm"])
+    let initialDta = [{task:"work at 10",id:1},{task:"sleep early",id:2}]
 
-    const onclick=()=>{
-    const k=document.getElementById("text")
+    const [data, dataUpdater] = useState(initialDta)
+    const [value, setValue] = useState("")
+
+    // function aaa(Id){
+    //     console.log(Id)
+    // }
+
     
-    if((k.value).length!==0){
-        const newdata=[...data,k.value]
-        dataUpdater(newdata)
-        k.value=""
-    }else{
-        alert("please fill the task to do")
-    }
-}
+    const onclick = () => {
+        // <FaTimes id ="cross" onChange={()=>deleteTask("hi")}/>
+        
 
-     
+        let code=(Math.random() + 1).toString(36).substring(7);
+        let newData={id:code,task:value}
+        console.log(newData)
+        
+        // const newdata = [...data, value]
+        dataUpdater([...data,newData])
+      
+        setValue("")
+    }
+    const onChangeHandler = (event) => {
+        let value = event.target.value
+        setValue(value)
+    }
+
+    const deleteTask=(id)=>{
+        console.log(id)
+        let r=data.map((ele)=>ele.id!==id)
+        console.log(r)
+        dataUpdater(data.filter((ele)=>ele.id!==id))
+        
+        
+        // data=data.map((ele)=>{ele.id!=id})
+        // dataUpdater(data)
+        
+    }
+
     return (
         <div className='class'>
         
-        {data.map(element => <h1 id ="p">{element}</h1>)}
         
-        <input id='text' type='text' placeholder="enter task to add"/>
+        
+            <div id ="innerDiv">
+           
+            <h2 id="head">Pending Tasks ({/*tasksRemain*/})</h2>
+                {data.map(element => <h1 id="p">{element.task} 
+                <ComplateButton  /><ClearIcon  id ="cross" onClick={()=>deleteTask(element.id)}/>
+                 </h1>)}
 
-        <input id ="btn" type='button' value='submit' onClick={onclick}/>
-        
-    </div>
+                <input id='text' type='text' value={value} placeholder="enter task to add" onChange={onChangeHandler} />
+
+                <input id="btn" type='button' value='submit' onClick={onclick} />
+            </div>
+
+
+
+        </div>
     )
 }
 
 
-   
